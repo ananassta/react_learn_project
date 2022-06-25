36d32d6f44ff001e8d171b9db542ed8b20792d2e
ananassta <anastasiyka_2015@mail.ru>
2022-06-25 18:18:48 +0300

    increment & managed component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [likes, setLikes] = useState(0)
+  // двучстороннее связывание 
+  // управляемый компонент
+  const [value, setValue] = useState('text in input')
+
+  function increment() {
+    setLikes(likes + 1)
+  }
+
+  function decrement() {
+    setLikes(likes - 1)
+  }
   return (
     <div className="App">
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input type='text' value={value} onChange={event => setValue(event.target.value)}/>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
 
     </div>
   );
