1ce9c65ff1763ef816497474b52e6b0102b59b0d
ananassta <anastasiyka_2015@mail.ru>
2022-06-25 18:50:04 +0300

    start working with posts| PostItem

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
-import ClassCounter from "./components/ClassCounter";
-import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import "./styles/App.css"
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
-      <Counter/>
-      <Counter/>
-
-      <ClassCounter/>
-
+      <PostItem/>
+      <PostItem/>
     </div>
   );
 }
