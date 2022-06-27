63acc4e06a13758b2691c04f9f683ec70e2c3a77
ananassta <anastasiyka_2015@mail.ru>
2022-06-27 18:33:21 +0300

    form | one controlled component

--- src/App.js
+++ src/App.js
@@ -10,18 +10,11 @@ function App() {
     {id: 2, title: 'JavaScript 2', body: "JavaScript"},
     {id: 3, title: 'JavaScript 3', body: "Язык программирования"}
   ])
-  const [title, setTitle] = useState()
-  const [body, setBody] = useState()
+  const [post, setPost] = useState({title: '', body: ''})
   const addNewPost = (e) => {
     e.preventDefault()
-    const newPost = {
-      id: Date.now(),
-      title,
-      body
-    }
-    setPosts([...posts, newPost])
-    setTitle()
-    setBody()
+    setPosts([...posts, {...post, id: Date.now()}])
+    setPost({title: '', body: ''})
   }
 
   return (
@@ -29,14 +22,14 @@ function App() {
       <form>
         {/* Управляемый компонент */}
         <MyInput 
-          value={title}
-          onChange={e => setTitle(e.target.value)} 
+          value={post.title}
+          onChange={e => setPost({...post, title: e.target.value})} 
           type="text" 
           placeholder="Название поста"
         />
         <MyInput 
-          value={body}
-          onChange={e => setBody(e.target.value)} 
+          value={post.body}
+          onChange={e => setPost({...post, body: e.target.value})} 
           type="text" 
           placeholder="Описание поста"
         />
