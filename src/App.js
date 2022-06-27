940f3c3299a38cae1b4866fc16787821780c1a29
ananassta <anastasiyka_2015@mail.ru>
2022-06-27 16:55:35 +0300

    postlist with 2 lists

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ function App() {
     {id: 2, title: 'JavaScript 2', body: "JavaScript"},
     {id: 3, title: 'JavaScript 3', body: "Язык программирования"}
   ])
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python 1', body: "Python - язык программирования"},
+    {id: 2, title: 'Python 2', body: "Python"},
+    {id: 3, title: 'Python 3', body: "Язык программирования"}
+  ])
   return (
     <div className="App">
       <PostList posts={posts} title={"Список постов 1"}/>
-      <PostList posts={posts} title={"Список постов 2"}/>
+      <PostList posts={posts2} title={"Список постов 2"}/>
     </div>
   );
 }
